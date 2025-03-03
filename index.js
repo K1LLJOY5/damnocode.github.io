8aad755d380e44d93e73c84beb3594c674e02f32
KJ <triananda45@gmail.com>
2025-03-03 14:54:22 +0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.menu-list a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 50,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+document.querySelector('.logo-link').addEventListener('click', function(e) {
+    e.preventDefault(); // Mencegah perpindahan default
+    const target = document.querySelector('#menu');
+    if (target) {
+        window.scrollTo({
+            top: target.offsetTop - 50, // Sesuaikan offset jika ada navbar tetap
+            behavior: 'smooth'
+        });
+    }
+});
